a9f0b8ac6ddf5fe7d364f53e4502f9d3366393c6
Douglinhas <daguiaralcantara@gmail.com>
2023-02-23 16:30:24 -0300

    git commit -m "feat: create http and errors helpers."

---
+++ src/presentation/helpers/http.ts
@@ -0,0 +1,15 @@
+import { HttpResponse } from "../protocols";
+
+export const badRequest = (error: Error): HttpResponse => {
+  return {
+    statusCode: 400,
+    body: error
+  }
+}
+
+export const ok = (data: any): HttpResponse => {
+  return {
+    statusCode: 200,
+    body: data
+  }
+}
